4dd31356cf3eb2120fe1ba36ba46754743af8cd2
Travis <travis@travis-ci.org>
2014-03-06 18:34:22 +0000

    Travis build 36 pushed to gh-pages

--- search/classes_73.js
+++ search/classes_73.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['sol',['Sol',['../classLevel_1_1Sol.html',1,'Level']]],
   ['spritegroup',['SpriteGroup',['../classRender_1_1SpriteGroup.html',1,'Render']]],
   ['stringutils',['StringUtils',['../classMisc_1_1StringUtils.html',1,'Misc']]]
 ];
