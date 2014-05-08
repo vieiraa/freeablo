439e9ab8a30a278b045921417beebc0fc9b5beea
Travis <travis@travis-ci.org>
2014-05-08 16:12:22 +0000

    Travis build 93 pushed to gh-pages

--- search/all_72.js
+++ search/all_72.js
@@ -4,6 +4,7 @@ var searchData=
   ['render',['Render',['../namespaceRender.html',1,'']]],
   ['renderer',['Renderer',['../classFARender_1_1Renderer.html',1,'FARender']]],
   ['renderlevel',['RenderLevel',['../structRender_1_1RenderLevel.html',1,'Render']]],
+  ['rendersettings',['RenderSettings',['../structRender_1_1RenderSettings.html',1,'Render']]],
   ['renderstate',['RenderState',['../classFARender_1_1RenderState.html',1,'FARender']]],
   ['room',['Room',['../classFALevelGen_1_1Room.html',1,'FALevelGen']]]
 ];
--- search/all_73.js
+++ search/all_73.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['sol',['Sol',['../classLevel_1_1Sol.html',1,'Level']]],
   ['spritegroup',['SpriteGroup',['../classRender_1_1SpriteGroup.html',1,'Render']]],
+  ['startupsettings',['StartupSettings',['../structStartupSettings.html',1,'']]],
   ['stringutils',['StringUtils',['../classMisc_1_1StringUtils.html',1,'Misc']]]
 ];
